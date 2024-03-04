d333b83daf5299b9c32c11c7b90a3abeefeaf1a2
Sereinay <jy.cao@bupt.edu.cn>
2024-03-04 21:48:49 +0800

    前端登录界面v0.1

---
+++ frontend/src/views/WelcomeView.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<template>
+  <div style="width: 100vw;height: 100vh;overflow: hidden;display: flex">
+    <div style="flex: 1;background-color: black">
+      <el-image style="width: 100%;height: 100%" fit="cover"
+          src="https://pic2.zhimg.com/v2-696b347aa5b02a943706d5de13dc6ec1_r.jpg"/>
+    </div>
+    <div class="welcome-title">
+      <div style="font-size: 30px;font-weight: bold">“因为山就在那里。”</div>
+      <div style="margin-top: 10px;margin-left: 60px">   ————George Herbert Leigh Mallory </div>
+      <div style="margin-top: 5px"></div>
+    </div>
+    <div class="right-card">
+      <router-view/>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+.right-card{
+  width:400px;
+  z-index: 1;
+  background-color: aliceblue;
+}
+.welcome-title{
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
+  color: #d5ffe8;
+}
+</style>
