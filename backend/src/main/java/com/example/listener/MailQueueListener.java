ace6c41f0f2eb3c07e63cd3b5910cc87b6c6a221
Sereinay <jy.cao@bupt.edu.cn>
2024-03-22 23:55:25 +0800

    完成了后端注册验证码的发送

---
+++ backend/src/main/java/com/example/listener/MailQueueListener.java
@@ -0,0 +1,46 @@
+package com.example.listener;
+
+import jakarta.annotation.Resource;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+@RabbitListener(queues = "mail")
+public class MailQueueListener {
+    @Resource
+    JavaMailSender sender;
+
+    @Value("${spring.mail.username}")
+    String username;
+
+    @RabbitHandler
+    public void sendMailMessage(Map<String, Object> data) {
+        String email = (String) data.get("email");
+        Integer code = (Integer) data.get("code");
+        String type = (String) data.get("type");
+        SimpleMailMessage message = switch (type) {
+            case "register" -> createMessage("欢迎加入我们",
+                    "您的验证码是：" + code + ",有效时间3min，为了保障安全，请勿向他人泄漏您的验证码。", email);
+            case "reset" -> createMessage("您的密码重置邮件",
+                    "你好，您正在进行重制密码操作，验证码：" + code + ",有效时间3min，如非本人操作，请忽略。", email);
+            default -> null;
+        };
+        if (message == null) return;
+        sender.send(message);
+    }
+
+    private SimpleMailMessage createMessage(String title, String content, String email) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setSubject(title);
+        message.setText(content);
+        message.setTo(email);
+        message.setFrom(username);
+        return message;
+    }
+}
--- backend/src/main/java/com/example/utils/Const.java
+++ backend/src/main/java/com/example/utils/Const.java
@@ -2,5 +2,8 @@ package com.example.utils;
 
 public class Const {
     public static final String JWT_BLACK_LIST = "jwt:blacklist:";
+
+    public static final String VERIFY_EMAIL_LIMIT = "verity:email:limits:";
+    public static final String VERIFY_EMAIL_DATA = "verity:email:data:";
     public static final int ORDER = -102;
 }
